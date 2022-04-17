7dd0e4acfba876c3eab4464546b773e5929978e5
csperando <csperando@gmail.com>
2022-04-17 10:01:03 -0400

    version 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -l
 
-# echo "::group::cd to target"
-# echo "There has to be a better way to deal with these directories..."
-# cd ..
-# cd ..
-# cd target
-# ls
-# echo "::endgroup::"
-
 echo "::group::linting directory: $1"
 java -jar ../../target/CFLint-1.5.0-all.jar -folder "$1"
 echo "::endgroup::"
@@ -16,6 +8,5 @@ echo "::group::format output"
 results=`cat cflint-result.html`
 results="${results//$'\n'/''}"
 results="${results//$'\r'/''}"
-echo $results
 echo "::set-output name=results::$results"
 echo "::endgroup::"

ea65eab2591c3d128a5dfa9fc293a727764b392e
csperando <csperando@gmail.com>
2022-04-16 23:42:28 -0400

    group outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -l
 
+echo "::group::cd to target"
 echo "There has to be a better way to deal with these directories..."
 cd ..
 cd ..
 cd target
+ls
+echo "::endgroup::"
 
-echo "linting directory: $1"
+echo "::group::linting directory: $1"
 java -jar CFLint-1.5.0-all.jar -folder "$1"
+echo "::endgroup::"

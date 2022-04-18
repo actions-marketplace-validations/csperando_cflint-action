029294f678c826486c19c8cb28d24a77107eaa7f
Coleman <coleman@originalyellow.com>
2022-04-18 13:41:58 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 echo "::group::linting directory: $1"
 
-echo java -jar /target/CFLint-1.5.0-all.jar -d -folder "$1"
+echo java -jar /target/CFLint-1.5.0-all.jar -d -text -folder "$1"
 java -jar /target/CFLint-1.5.0-all.jar -d -folder "$1"
 
-echo results=`cat cflint-result.html`
-results=`cat cflint-result.html`
+echo results=`cat cflint-result.txt`
+results=`cat cflint-result.txt`
 # results="${results//$'\n'/''}"
 # results="${results//$'\r'/''}"
 

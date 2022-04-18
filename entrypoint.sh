46f47696a4458131ddeaee0835d9b12b6785f68b
Coleman <coleman@originalyellow.com>
2022-04-18 15:34:34 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh -l
 
+# get diff from codebase ...
+
 echo "::group::linting directory: $1"
 echo java -jar /target/CFLint-1.5.0-all.jar -q -folder "$1"
 java -jar /target/CFLint-1.5.0-all.jar -q -folder "$1"
 echo "::endgroup::"
-
-# echo "::group::format results"
-# echo results=`cat cflint-result.html`
-# results=`cat cflint-result.html`
-# results="${results//$'\n'/' '}"
-# echo "::set-output name=results::$results"
-# echo "::endgroup::"

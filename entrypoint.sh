5b8c7986894dba180e2f986c50407d19e02dcb90
Coleman <coleman@originalyellow.com>
2022-04-18 12:52:44 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
 echo "::group::linting directory: $1"
+
 java -jar /target/CFLint-1.5.0-all.jar -folder "$1"
+
 cd /target
 ls
-echo "::endgroup::"
 
-echo "::group::format output"
 results=`cat /target/cflint-result.html`
 results="${results//$'\n'/''}"
 results="${results//$'\r'/''}"
+
 echo "::set-output name=results::$results"
+
 echo "::endgroup::"

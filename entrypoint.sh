865e6471f40ff7b354f72f8a1cd1e749d61426a8
Coleman <coleman@originalyellow.com>
2022-04-18 14:10:12 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ echo java -jar /target/CFLint-1.5.0-all.jar -q -folder "$1"
 java -jar /target/CFLint-1.5.0-all.jar -q -folder "$1"
 echo "::endgroup::"
 
-echo "::group::format results"
-echo results=`cat cflint-result.html`
-results=`cat cflint-result.html`
-results="${results//$'\n'/' '}"
-echo "::set-output name=results::$results"
-echo "::endgroup::"
+# echo "::group::format results"
+# echo results=`cat cflint-result.html`
+# results=`cat cflint-result.html`
+# results="${results//$'\n'/' '}"
+# echo "::set-output name=results::$results"
+# echo "::endgroup::"
